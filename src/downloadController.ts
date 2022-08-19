7bbce3d0384843eff1847e18641c49a639771f8d
varyuhi <varyadaser@gmail.com>
2022-08-19 16:24:22 +0400

    working on the async await part rn

--- src/downloadController.ts
+++ src/downloadController.ts
@@ -2,17 +2,23 @@ import fs from 'fs';
 import { Request, Response } from "express";
 import { downloadImage } from "./downloadImg";
 
+
 const checkIfImgExists = (req: Request, res: Response) => {
     let url = req.query.url as string;
     let fileName = encodeURIComponent(url);
-    const destFolder = __dirname + '/images';
+    const destFolder = `${__dirname}/images`;
 
-    fs.readdir(destFolder, async (err: any, files: string[]) => {
-        if (!(files.includes(fileName))) {
-            await downloadImage(fileName, destFolder)
-        }
-        res.status(200).sendFile(__dirname + `/images/${fileName}`)
-    })
-};
+    try {
+        fs.readdir(destFolder, async (err: any, files: string[]) => {
+            if (!(files.includes(fileName))) {
+                await downloadImage(fileName, destFolder).then(()=>console.log("qashec"))
+            }
+            res.status(200).sendFile(`${__dirname}/images/${fileName}`)
+            console.log("uxarkec")
+        })
 
-export { checkIfImgExists }
+    } catch (err) {
+        throw err;
+    }
+}
+export { checkIfImgExists };
